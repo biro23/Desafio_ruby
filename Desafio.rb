18bf4110449febd4f5b04052ee30989ee9bfd09c
biro23 <121694408+biro23@users.noreply.github.com>
2023-09-06 10:29:34 -0300

    Desafio ruby criando meu primeiro programa

---
+++ Desafio.rb
@@ -0,0 +1,12 @@
+# o usuario precisa devera digitar seu nome
+print "Qual e o seu nome:    "
+nome = gets.chomp
+# o usuario precisa digitar o sobrenome
+print "Qual e o sobrenome:   "
+sobrenome = gets.chomp
+# o usuario digitar sua idade
+print "Qual e a sua idade:   "
+idade = gets.chomp
+# tudo isso impresso em uma unica frase.
+puts "O resultado de tudo isso e :  "
+puts "#{nome}, #{sobrenome}, #{idade}"
